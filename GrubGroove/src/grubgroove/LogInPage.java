c735c7dfbefa516a7da3a393088908159ad1e46f
Gieb Mier <128208941+giebm@users.noreply.github.com>
2023-05-03 21:49:31 +0800

    Add files via upload

---
+++ GrubGroove/src/grubgroove/LogInPage.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author Gieb Mier
+ */
+public class LogInPage {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        System.out.print("This is the log-in page!");
+    }
+}
